effc498f774f5d8101bb20c9505e0c31f792e3ee
Dino Scheidt <github@dino-scheidt.de>
2021-01-03 05:13:47 +0100

    ✨ Real-Time Loop Working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import 'firebase/database';
-import { SuspenseWithPerf, useDatabase, useDatabaseObjectData } from 'reactfire';
+import React from "react";
+import "firebase/database";
+import {
+  SuspenseWithPerf,
+  useDatabase,
+  useDatabaseObjectData,
+} from "reactfire";
 
-import logo from './logo.svg';
-
+import logo from "./logo.svg";
 
 /** Watching a single Object inside Firebase Real-Time Database */
 function Tap() {
-
   type tapList = {
-    isYummy: boolean
-  }
+    counter: number;
+  };
 
   const ref = useDatabase().ref("list");
 
@@ -20,34 +22,64 @@ function Tap() {
   const data = document.data as tapList;
   const status = document.status;
 
-  // easily check the loading status
-  if (status === 'loading') {
-    return <p>Fetching burrito flavor...</p>
-  }
+  // Display Value
+  const display = () => {
+    if (status === "loading") {
+      return "Loading...";
+    }
+    return data.counter;
+  };
 
-  return <p>The burrito is {data.isYummy ? 'good' : 'bad'}</p>;
+  // Animation from loading State
+  const loadingClass = () => {
+    if (status === "loading") {
+      return "text-6xl";
+    }
+    return "text-9xl";
+  };
+
+  return (
+    <span
+      className={
+        "self-center text-gray-400 opacity-60 transition-all " + loadingClass()
+      }
+    >
+      {display()}
+    </span>
+  );
 }
 
-
 function App() {
+  const ref = useDatabase().ref("list");
+
+  function handleClick(e: React.MouseEvent<HTMLElement>) {
+    ref.once("value", function (snapshot) {
+      snapshot.ref.update({
+        counter: snapshot.val().counter + 1,
+      });
+    });
+
+    e.preventDefault();
+    console.log("The link was clicked.");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p >
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <SuspenseWithPerf
+          fallback={"loading burrito status..."}
+          traceId={"load-burrito-status"}
         >
-          Learn React
-        </a>
-        <SuspenseWithPerf fallback={'loading burrito status...'} traceId={'load-burrito-status'}>
-          <Tap />
+          <button
+            className="NeuButton w-96 h-96 flex justify-center cursor-pointer"
+            onClick={handleClick}
+          >
+            <Tap />
+          </button>
         </SuspenseWithPerf>
+        <div className="text-base italic p-10 text-gray-400">
+          by Dino Scheidt
+        </div>
       </header>
     </div>
   );
