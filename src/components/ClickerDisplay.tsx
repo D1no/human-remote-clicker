bad589057741c8f07344c9a975d5bb16cac8049d
Dino Scheidt <github@dino-scheidt.de>
2021-01-25 22:06:39 +0100

    💄 Make ClickerDisplay Component composable for animation

--- src/components/ClickerDisplay.tsx
+++ src/components/ClickerDisplay.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 type ClickerDisplayProps = {
-  /** Text to be shown at the center of the display. */
+  /** Content of SVG text to be shown at the center of the display. */
   text?: string;
+  /** Additional SVG text className property. */
+  textClassName: string;
+  /** Additonal SVG cricle path className property. */
+  circleClassName: string;
   /** Displays a background color behind layout sections for debugging. */
   debugView: boolean;
 } & typeof defaultProps;
 
 const defaultProps = {
+  textClassName: "text-white text-3xl",
+  circleClassName: "text-red-600",
   debugView: false,
 };
 
-/** Displays an svg element proportionally centered with the text set at the center. */
+/** Displays an svg element proportionally centered with text set at the center. Has default styles.*/
 const ClickerDisplay = (props: ClickerDisplayProps) => {
   return (
     <div
@@ -23,20 +29,22 @@ const ClickerDisplay = (props: ClickerDisplayProps) => {
       <svg
         viewBox="0 0 140 140"
         preserveAspectRatio="xMinYMin meet"
-        className={
-          "fill-current text-red-600 h-4/5 max-w-4/5" +
-          (props.debugView ? " bg-pink-400" : "")
-        }
+        className={"h-4/5 max-w-4/5 " + (props.debugView ? " bg-pink-400" : "")}
       >
         {/* ToDo: <g> tag is not vertically centered inside the ViewBox, effecting very long displays like pixel 2 xl */}
         <g>
-          <circle r="50%" cx="50%" cy="50%" />
+          <circle
+            r="50%"
+            cx="50%"
+            cy="50%"
+            className={"fill-current " + props.circleClassName}
+          />
           <text
             x="50%"
             y="50%"
             text-anchor="middle"
             dy="0.3em"
-            className={"fill-current text-white text-3xl"}
+            className={"fill-current " + props.textClassName}
           >
             {props.text}
           </text>
